f606ef5e810a4b77ebce4da52e0f1a0472a6e6a7
tsihnavy99 <tsihnavy99@gmail.com>
2024-11-25 21:43:20 +0900

    📝 utils.js testSolution 수정
    solution 실행 시간 측정 방법 변경
    
    기존: solution 1회만 실행, 실행 전후 시간 비교
    변경: solution 시간 측정 없이 5회 실행(컴파일러 최적화), 실제 측정은 20회 실행하며 평균 실행시간 계산

--- utils.js
+++ utils.js
@@ -2,20 +2,39 @@ function testSolution(ex_arr, answer_arr, solution) {
   ex_arr.forEach((ex, idx) => {
     if (answer_arr[idx] !== null) {
       try {
-        const startTime = performance.now();
+        // 웜업 (JIT 컴파일러 최적화 -> 첫 실행은 최적화가 없기 때문에 상대적으로 느림)
+        for (let i = 0; i < 5; i++) {
+          solution(...ex);
+        }
+
+        // 실제 측정
+        const times = [];
+        // 실제 측정 횟수가 적으면 실행시간 신뢰도가 떨어지고 많으면 test 실행 시간이 늘어남
+        for (let i = 0; i < 20; i++) {
+          const startTime = performance.now();
+          solution(...ex);
+
+          const endTime = performance.now();
+          times.push(endTime - startTime);
+        }
+
+        // 최소/최대값 제외 및 실행 평균시간 계산
+        times.sort((a, b) => a - b);
+        const trimmedTimes = times.slice(1, -1);
+        const avgTime =
+          trimmedTimes.reduce((a, b) => a + b) / trimmedTimes.length;
+
+        // 최종 실행 및 정확성 체크
         const result = solution(...ex);
-
-        const endTime = performance.now();
-        const duration = endTime - startTime;
-
         const answer = answer_arr[idx];
+
         console.log("Result >>", result);
         console.log("Answer >>", answer);
         console.log(
           `ex${idx + 1} >>`,
           result.toString() === answer.toString() ? "🎉 CORRECT" : "💦 WRONG",
           "/ ⏱️",
-          ` ${duration.toFixed(2)}ms`
+          ` ${avgTime.toFixed(4)}ms`
         );
       } catch (e) {
         console.log("🚨🚨🚨 solution 실행 중 에러가 발생했습니다 🚨🚨🚨");
